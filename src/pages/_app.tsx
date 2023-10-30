1e1a2d7b6199508037684dcc70e0975b6ad1dd2e
delver <dima.plugaturevchik@gmail.com>
2023-10-30 23:56:54 +0200

    Added Header with the search bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import SearchAppBar from "@/components/SearchAppBar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <SearchAppBar />
+    <Component {...pageProps} />
+  </>
 }
