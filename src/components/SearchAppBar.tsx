9afb945d5d599a6d24e2dea4ed26d00f3da892bf
delver <dima.plugaturevchik@gmail.com>
2023-10-30 23:59:32 +0200

    Made a trial to implement pagination

--- src/components/SearchAppBar.tsx
+++ src/components/SearchAppBar.tsx
@@ -8,7 +8,6 @@ import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import {log} from "util";
 import Link from "next/link";
 
 const Search = styled('div')(({ theme }) => ({
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,38 @@
 import {GetStaticProps, InferGetStaticPropsType, NextPage} from "next";
 import axios from "axios";
-import ProductCard from "@/components/ProductCard";
+import ProductCard from "@/components/product/ProductCard";
+import usePagination from "@/hooks/usePagination";
+import Head from "next/head";
+import PaginationBar from "@/components/PaginationBar";
+import {useCallback, useState} from "react";
+import Link from "next/link";
 
 const apiUrl = "https://dummyjson.com/products";
+const productsCountPerPage = 10;
 
-const fetchProducts = async (apiUrl: string) => {
+let total: number;
+let limit: number;
+
+const fetchProducts = async (apiUrl: string, skip: number, count: number = productsCountPerPage) => {
     let result: Product[] = [];
 
-    const productApiOutline = await axios.get<ProductApiOutline>(apiUrl);
-    const limit = productApiOutline.data.limit;
-    const total = productApiOutline.data.total;
-    let skip = productApiOutline.data.skip;
-
     try {
-        while (true) {
-            const response = await axios.get<{products: Product[] }>(`${apiUrl}?skip=${skip}`);
-
-            const products = response.data.products;
-
-            if (products.length === 0) {
-                break;
-            }
-
-            result = result.concat(products);
-            skip += limit;
-
-            if (skip >= total) {
-                break;
-            }
-        }
+        const response =
+            await axios.get<{ products: Product[] }>(`${apiUrl}?skip=${skip}`);
+        result = response.data.products;
     } catch (err) {
         console.error("Error fetching products:", err);
     }
 
-    return result;
+    return result.slice(0, count);
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
-    const products = await fetchProducts(apiUrl);
+export const getStaticProps: GetStaticProps = async () => {
+    const productApiOutline = await axios.get<ProductApiOutline>(apiUrl);
+    limit = productApiOutline.data.limit;
+    total = productApiOutline.data.total;
+
+    const products = await fetchProducts(apiUrl, 0);
 
     return {
         props: {
@@ -47,20 +42,33 @@ export const getStaticProps: GetStaticProps = async (context) => {
 };
 
 const AllProducts: NextPage = ({products}: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const totalPagesCount = Math.ceil(total / productsCountPerPage);
+    const [skip, setSkip] = useState(0);
+
+    const handleProductsRendering = useCallback(() =>
+        fetchProducts(apiUrl, skip), []);
+
     return (<>
-        <h1>All Products</h1>
-        {products.map((product: Product) => {
-            return <ProductCard
-                title={product.title}
-                key={product.id}
-                photo={product.images[0]}
-                brand={product.brand}
-                category={product.category}
-                discount={product.discountPercentage}
-                price={product.price}
-                rating={product.rating}
-            />;
-        })}
+        <Head>
+            <title>All Products</title>
+        </Head>
+        <main style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+            {products.map((product: Product) => {
+                return <Link key={product.title} href="./product/product-details/">
+                    <ProductCard
+                        title={product.title}
+                        key={product.id}
+                        photo={product.images[0]}
+                        brand={product.brand}
+                        category={product.category}
+                        discount={product.discountPercentage}
+                        price={product.price}
+                        rating={product.rating}
+                    />
+                </Link>;
+            })}
+        </main>
+        <PaginationBar data={products} totalPagesCount={totalPagesCount}/>
     </>);
 };
 
