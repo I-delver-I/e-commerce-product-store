c43a38a8d76050c97721ee4ae8cbb65bae5752b1
Delver <89275215+Scarmaing-Whebrolted@users.noreply.github.com>
2023-10-31 11:10:39 +0200

    Partially implemented the project (#12)
    * Created product card
    
    * Implemented product card and integrated the provided API to project
    
    * Added packages, deleted useless code
    
    * Added Header with the search bar
    
    * Made minor changes
    
    * Made a trial to implement pagination
    
    * Made minor changes and began to implement Product details page

---
+++ src/components/product/ProductCard.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import {CardActionArea} from '@mui/material';
+import ProductCardContent from "@/components/product/ProductCardContent";
+import Link from 'next/link';
+
+export default function ProductCard({photo, ...props}: ProductCardType) {
+    return (
+        <Card sx={{
+            width: 330, height: 450, margin: "20px", transition: "transform 0.2s ease",
+            "&:hover": {
+                transform: "scale(1.05)",
+                borderColor: "green",
+            }
+        }}>
+            <CardActionArea onClick={() => <Link href="/product/product-details" />} sx={{
+                height: "100%", width: "100%",
+                boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)"
+            }}>
+                <CardMedia
+                    sx={{
+                        height: "50%", width: "100%", objectFit: "contain", border: "2px solid green",
+                        transition: "border-color 0.6s",
+                        "&:hover": {
+                            borderColor: "green",
+                            boxShadow: "0 0 10px rgba(0, 128, 0, 0.5)"
+                        }
+                    }}
+                    component="img"
+                    src={photo}
+                    alt="green iguana"
+                />
+                <ProductCardContent
+                    title={props.title}
+                    brand={props.brand}
+                    category={props.category}
+                    discount={props.discount}
+                    price={props.price}
+                    rating={props.rating}/>
+            </CardActionArea>
+        </Card>
+    );
+}
